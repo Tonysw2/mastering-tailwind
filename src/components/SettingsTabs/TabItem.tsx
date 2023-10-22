b9b3736f3f1770e97940f81938b3b35f81f5d2e3
Anthony Ribeiro da Mata <anthonyrmataa@gmail.com>
2023-10-22 16:42:56 -0300

    fix: adjust Tailwind CSS styling order for Vercel deployment

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -13,9 +13,9 @@ export function TabItem({ value, title, isSelected = false }: Props) {
   return (
     <Tabs.Trigger
       value={value}
-      className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700 dark:text-zinc-400 dark:data-[state=active]:text-violet-300"
+      className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 outline-none data-[state=active]:text-violet-700 hover:text-violet-700 dark:text-zinc-400 dark:data-[state=active]:text-violet-300"
     >
-      <span className="whitespace-nowrap rounded hover:text-violet-300 group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+      <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4 hover:text-violet-300">
         {title}
       </span>
 
